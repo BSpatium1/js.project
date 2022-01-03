f2d92b2eb123e26491e0ffbbe29e63f1a4dc861a
Artem <Artem-golev@list.ru>
2022-01-03 20:05:56 +0300

    ничего не работает

--- script/script.js
+++ script/script.js
@@ -1,25 +1,55 @@
+//const numberOfFilms = +prompt("Сколько фильмов вы посмотрели ?");
+
+//prerconalMovieDB = {
+// count: numberOfFilms,
+// movies:{},
+// actors:{},
+//  genrse:[],
+//  privat: false
+//};
+
+//const a = prompt("Один из последних просмотренных фильмов ?"),
+// b = prompt("Оцените его : "),
+//c = prompt("Один из последних просмотренных фильмов ?"),
+//d = prompt("Оцените его : ");
+
+//prerconalMovieDB.movies[a] = b;
+//prerconalMovieDB.movies[c] = d;
+
+//console.log(prerconalMovieDB);
+
+
 const numberOfFilms = +prompt("Сколько фильмов вы посмотрели ?");
 
-prerconalMovieDB = {
+prersonalMovieDB = {
     count: numberOfFilms,
-    movies:{},
-    actors:{},
-    genrse:[],
+    movies: {},
+    actors: {},
+    genrse: [],
     privat: false
 };
 
-const a = prompt("Один из последних просмотренных фильмов ?"),
-      b = prompt("Оцените его : "),
-      c = prompt("Один из последних просмотренных фильмов ?"),
-      d = prompt("Оцените его : ");
+for (let i = 0; i < 2; i++) {
+     const a = prompt("Один из последних просмотренных фильмов ?"," ");
+     const b = prompt("Оцените его : "," ");
+           
+if (a != null && b != null && a != " " && b != " " && a.lenght < 50) {
+        prersonalMovieDB[a] = b;
+        console.log("Done");
+    } else {
+        console.log("Error");
+        i--;
+    }
+}
 
-prerconalMovieDB.movies[a] = b;
-prerconalMovieDB.movies[c] = d;
+if (prersonalMovieDB.count < 10) {
+    console.log("Довольно мало фильмов");
+} else if (prersonalMovieDB.count >= 10 && prerconalMovieDB.count < 30) {
+    console.log("Вы классический зритель");
+} else if (prersonalMovieDB.count > 30) {
+    console.log("Вы киноман!");
+} else {
+    console.log("ошибка!");
+}
 
-console.log(prerconalMovieDB);
-
-
-
-
-
-
+console.log(prersonalMovieDB);
